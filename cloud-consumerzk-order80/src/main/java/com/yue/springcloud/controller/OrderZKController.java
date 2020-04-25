9d3aa39001e4dab7668717ea5d58754e64be77d4
zengpengyue <zengpengyue@hongmeng-info.com>
2020-04-25 22:38:02 +0800

    feature:搭建并使用consul作为注册中心

---
+++ cloud-consumerzk-order80/src/main/java/com/yue/springcloud/controller/OrderZKController.java
@@ -0,0 +1,22 @@
+package com.yue.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderZKController {
+    public static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/zk")
+    public String paymentInfo(){
+        return restTemplate.getForObject(INVOKE_URL + "/payment/zk",String.class);
+    }
+}
