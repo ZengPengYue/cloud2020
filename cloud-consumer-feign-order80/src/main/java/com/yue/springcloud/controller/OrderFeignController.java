fde6ff35cb90cbbcf503df1e0193a554c247e121
zengpengyue <zengpengyue@hongmeng-info.com>
2020-04-27 23:08:54 +0800

    feature:了解和使用OpenFeign;测试连接超时和日志打印配置

---
+++ cloud-consumer-feign-order80/src/main/java/com/yue/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,29 @@
+package com.yue.springcloud.controller;
+
+import com.yue.springcloud.entities.CommonResult;
+import com.yue.springcloud.entities.Payment;
+import com.yue.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderFeignController {
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable(value = "id") Long id){
+        return paymentFeignService.getPaymentById(id);
+    }
+
+    @GetMapping(value = "/consumer/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        //openfeign-ribbon，客户端调用默认等待1秒钟
+        return paymentFeignService.paymentFeignTimeout();
+    }
+}
